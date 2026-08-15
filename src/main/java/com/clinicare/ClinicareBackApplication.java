6058109f9835de140ce8e227c130b07c0dd9a2c6
Mohamed Gharbi <mohamed.gharbyy@gmail.com>
2026-08-16 01:37:49 +0200

    initial commit

---
+++ src/main/java/com/clinicare/ClinicareBackApplication.java
@@ -0,0 +1,13 @@
+package com.clinicare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicareBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicareBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clinicare/ClinicareBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clinicare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicareBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
